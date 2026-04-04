c72d30d97dbaf54a1b06ad6e5928b3f4b5996126
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-04 23:33:24 +0530

    basic java programs

---
+++ JAVA_A38/Speed.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Speed {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double D = sc.nextDouble();
+		double T = sc.nextDouble();
+		if(T<1 || T>60){
+			System.out.println("Invalid data");
+		return;
+		}
+
+		double speed = (D/T)*18/5;
+		System.out.println(speed);
+		
+	}
+}
---
+++ JAVA_A38/TwoSum.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class TwoSum {
+
+    public static int[] Sum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 5, 6, 9};
+        int target = 5;
+
+        int[] ans = Sum(arr, target);
+
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}

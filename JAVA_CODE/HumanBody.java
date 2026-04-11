a1a33c76194cb83b214ec1a98b07a2b407103b05
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-11 23:28:36 +0530

    Code: byte datatype example

---
+++ JAVA_CODE/HumanBody.java
@@ -0,0 +1,18 @@
+class HumanBody{
+	public static void main(String[] args){
+		String name = "Human Body";
+		byte hands = 2;
+		byte legs = 2;
+		byte fingers = 20;
+		byte teeths = 32;
+		byte eyes = 2;
+
+
+		System.out.println(name);
+		System.out.println("Hands :"+hands);
+		System.out.println("legs);
+		System.out.println(fingers);
+		System.out.println(eyes);
+		
+	}
+}
---
+++ JAVA_CODE/MyVillage.java
@@ -0,0 +1,18 @@
+class MyVillage{
+
+	public static void main(String[]args){
+		short population = 1200;
+		short vehicles = 200;
+		short womens = 400;
+		short mens = 600;
+		short children = 200;
+		
+
+		System.out.println("approx population : " + population);
+		System.out.println("avg vehicles : " + vehicles);
+		System.out.println("avg womens : " + womens);
+		System.out.println("avg mens : " + mens);
+		System.out.println("avg children:" + children);
+		
+	}
+}

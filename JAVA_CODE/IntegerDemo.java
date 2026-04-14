c2b761e0751eab4f89640669da806364979577f3
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-14 23:46:29 +0530

    Code: interger example min and max range

---
+++ JAVA_CODE/IntegerDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class IntegerDemo{
+	public static void main(String[] args){
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+	}
+}
+
+/*
+
+OUTPUT:
+
+-2147483648
+2147483647
+
+
+*/

b8c392056880c143ee66782862736db8f7134a3b
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-15 08:56:53 +0530

    Code: separated by brackets and parenthesses

--- JAVA_CODE/ArraysInput.java
+++ JAVA_CODE/ArraysInput.java
@@ -54,6 +54,10 @@ class ArraysInput{
 
 // input array when size of array is not given:
 
+
+/*
+
+
 import java.util.Scanner;
 import java.util.ArrayList;
 // import java.util.*;
@@ -83,5 +87,40 @@ class ArraysInput{
 			System.out.print(a+ " ");
 		}
 		sc.close();
+	}
+}
+
+*/
+
+// when user give the array input like {1,2,3,4,5} or [1,2,3,4,5] it can be {1 2 3 4 5}, [1 2 3 4 5]
+
+import java.util.Scanner;
+import java.util.ArrayList;
+// import java.util.*;
+
+class ArraysInput{
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		
+		if(input.startsWith("{") && input.endsWith("}") || input.startsWith("[") && input.endsWith("]")){
+			input = input.substring(1, input.length()-1);
+		}
+	
+		//String []ch = input.split(" ");
+		String []ch = input.split(",");
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		for(String token: ch){
+			int num = Integer.parseInt(token);
+			arr.add(num);
+		}
+
+		for(int a: arr){
+			System.out.print(a+ " ");
+		}
+		sc.close();
+
 	}
 }

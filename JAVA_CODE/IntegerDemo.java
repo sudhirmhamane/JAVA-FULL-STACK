4647c49ba43e7fea30eaea4969ce3ea750f1a5b2
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-15 08:27:19 +0530

    Code: made some logic changes

--- JAVA_CODE/IntegerDemo.java
+++ JAVA_CODE/IntegerDemo.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 class IntegerDemo{
 	public static void main(String[] args){
 		System.out.println(Integer.MIN_VALUE);
--- JAVA_CODE/ShortEx.java
+++ JAVA_CODE/ShortEx.java
@@ -1,13 +1,16 @@
 class ShortEx{
 	public static void main(String[]args){
 		short a = 10;
-		byte b = a;// error: incompatible types: possible lossy conversion from short to byte
+		//byte b = a;// error: incompatible types: possible lossy conversion from short to byte
 		int c = a;
+		System.out.println(c);
 		long d = a;
+		System.out.println(d);
 		float e = a;
-		double f = a; 
-		char g = a; // error: incompatible types: possible lossy conversion from short to char
-		boolean h = a; // incompatible types: short cannot be converted to boolean 
-		
+		System.out.println(e);
+		double f = a;
+		System.out.println(f);
+		// char g = a; // error: incompatible types: possible lossy conversion from short to char
+		// boolean h = a; // incompatible types: short cannot be converted to boolean 
 	}
 }

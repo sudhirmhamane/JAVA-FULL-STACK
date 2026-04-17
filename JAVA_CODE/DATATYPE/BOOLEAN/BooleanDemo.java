10804c14912ca2b3643a8a287e65369396f2b0a5
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-18 00:32:11 +0530

    notes for boolean datatype

---
+++ JAVA_CODE/DATATYPE/BOOLEAN/BooleanDemo.java
@@ -0,0 +1,34 @@
+/*
+	1) boolean is a negative datatype.
+	2) It's memory allocation is 1 bit.
+	3) Default value of boolean is false.
+	4) It will help us to create boolean type of container where we can store.
+	   boolean value, i.e true/false.
+	5) boolean does not have range, because it store only two values that(true/false).
+
+	---
+
+	Datatype Table:
+
+	byte 	  +   boolean 	=>  Compile Time Error
+	short 	  +   boolean 	=>  Compile Time Error
+	int 	  +   boolean 	=>  Compile Time Error
+	long 	  +   boolean 	=>  Compile Time Error
+	float 	  +   boolean 	=> 	Compile Time Error
+	double 	  +   boolean 	=>  Compile Time Error
+	boolean   +   boolean 	=> 	Compile Time Error / Depends on operator.
+	char 	  +   boolean 	=>  Compile Time Error
+
+
+
+*/
+
+class BooleanDemo{
+
+	public static void main(String[] args){
+
+		// boolean does not have range, because it store only two values that(true/false).
+		// System.out.println(Boolean.MIN_VALUE);
+		// System.out.println(Boolean.MAX_VALUE);
+	}
+}

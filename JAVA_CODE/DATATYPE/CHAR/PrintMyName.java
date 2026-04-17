9414b2fd12423a8a03a702b3bbfaed1fc33ca9c1
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-18 00:32:56 +0530

    A java program to print marathi name using unicode

---
+++ JAVA_CODE/DATATYPE/CHAR/PrintMyName.java
@@ -0,0 +1,21 @@
+class PrintMyName{
+
+	public static void main(String[] args) {
+
+        char ch1 = '\u0938';
+	System.out.print(ch1);
+
+	char ch2 = '\u0941';
+	System.out.print(ch2);
+
+	char ch3 = '\u0927';
+	System.out.print(ch3);
+
+	char ch4 = '\u0940';
+	System.out.print(ch4);
+
+	char ch5 = '\u0930';
+	System.out.print(ch5);
+
+    }
+}

263d9c091ce94eee7f97fea6c98468c95258ea1c
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-18 21:40:37 +0530

    Code: print string using for loop

---
+++ JAVA_CODE/PRACTICE/StringPractice.java
@@ -0,0 +1,14 @@
+
+class StringPractice{
+
+	public static void main(String [] args){
+
+		String str = "hello";
+
+		for(int i=0; i<str.length(); i++){
+			System.out.print(str.charAt(i));
+		}
+		System.out.println();
+	}
+
+}

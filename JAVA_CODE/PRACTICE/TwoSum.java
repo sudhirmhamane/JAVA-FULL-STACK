648b389ce340201775b467ea91e3b4a876451e48
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-20 23:40:25 +0530

    Code: two sum problem solve using nested loop

--- JAVA_CODE/PRACTICE/TwoSum.java
+++ JAVA_CODE/PRACTICE/TwoSum.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class TwoSum {
 
     public static int[] Sum(int[] arr, int target) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == target) {

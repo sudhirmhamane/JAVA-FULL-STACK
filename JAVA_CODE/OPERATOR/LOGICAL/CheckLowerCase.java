e6c26ce58682e868c7ab52bb29ae707088ccf242
sudhirM45 <sudhirmhamane908@gmail.com>
2026-04-29 00:40:30 +0530

    Java Code: check for lower case and upper case

---
+++ JAVA_CODE/OPERATOR/LOGICAL/CheckLowerCase.java
@@ -0,0 +1,16 @@
+// wap to check given char is lowercase?
+
+class CheckLowerCase{
+
+	public static void main(String[] args){
+		char ch = 'a';
+		String res = (ch >= 'a' && ch <= 'z') ? "LOWERCASE" : "NOT LOWERCASE";
+		System.out.println(res);
+	}
+}
+
+/*
+
+o/p => LOWERCASE
+
+*/
---
+++ JAVA_CODE/OPERATOR/LOGICAL/CheckUpperCase.java
@@ -0,0 +1,14 @@
+class CheckUpperCase{
+	
+	public static void main(String args[]){
+		char ch = 'A';
+		String res = (ch >= 'A' && ch <= 'Z') ? "UPPERCASE" : "LOWERCASE";
+		System.out.println(res);
+	}
+}
+
+/*
+
+o/p => UPPERCASE
+
+*/
